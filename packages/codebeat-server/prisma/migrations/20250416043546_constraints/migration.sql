38d166175412f52a2689447dcf635f45b0e48c6c
result17 <50240405+result17@users.noreply.github.com>
2025-04-16 14:15:13 +0800

    feat: change database

---
+++ packages/codebeat-server/prisma/migrations/20250416043546_constraints/migration.sql
@@ -0,0 +1,7 @@
+-- Add unsupported database features
+ALTER TABLE "Heartbeats" ADD CONSTRAINT "Lines_non_negative" CHECK ("lines" IS NULL OR "lines" >= 0),
+ADD CONSTRAINT "Lineno_valid_range" CHECK (
+    "lineno" IS NULL OR 
+    ("lineno" >= 0 AND ("lines" IS NULL OR "lineno" <= "lines"))
+),
+ADD CONSTRAINT "Time_order" CHECK ("recvAt" >= "sendAt" AND "createdAt" >= "recvAt")
