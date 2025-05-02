34a54711fa61019aa11df36002d125335dcef31f
result17 <rescp17@gmail.com>
2025-05-02 21:40:33 +0800

    chore: install d3

--- packages/codebeat-ext/src/generated/meta.ts
+++ packages/codebeat-ext/src/generated/meta.ts
@@ -2,83 +2,85 @@
 // @see https://github.com/antfu/vscode-ext-gen
 
 // Meta info
-export const publisher = 'result17'
-export const name = 'codbeat-ext'
-export const version = '0.0.0'
-export const displayName = 'CodeBeat-ext'
+export const publisher = "result17"
+export const name = "codebeat-ext"
+export const version = "0.0.0"
+export const displayName = "CodeBeat-ext"
 export const description = undefined
 export const extensionId = `${publisher}.${name}`
 
 /**
  * Type union of all commands
  */
-export type CommandKey =
-  | 'codeBeat.toggle'
+export type CommandKey = 
+  | "codeBeat.toggle"
 
 /**
- * Commands map registed by `result17.codbeat-ext`
+ * Commands map registed by `result17.codebeat-ext`
  */
 export const commands = {
   /**
    * Toggle
    * @value `codeBeat.toggle`
    */
-  codeBeatToggle: 'codeBeat.toggle',
+  codeBeatToggle: "codeBeat.toggle",
 } satisfies Record<string, CommandKey>
 
 /**
  * Type union of all configs
  */
-export type ConfigKey =
-  | 'codeBeat.enable'
+export type ConfigKey = 
+  | "codeBeat.enable"
 
 export interface ConfigKeyTypeMap {
-  'codeBeat.enable': boolean
+  "codeBeat.enable": boolean,
 }
 
 export interface ConfigShorthandMap {
-  codeBeatEnable: 'codeBeat.enable'
+  codeBeatEnable: "codeBeat.enable",
 }
 
 export interface ConfigShorthandTypeMap {
-  codeBeatEnable: boolean
+  codeBeatEnable: boolean,
 }
 
 export interface ConfigItem<T extends keyof ConfigKeyTypeMap> {
-  key: T
-  default: ConfigKeyTypeMap[T]
+  key: T,
+  default: ConfigKeyTypeMap[T],
 }
 
+
 /**
- * Configs map registered by `result17.codbeat-ext`
+ * Configs map registered by `result17.codebeat-ext`
  */
 export const configs = {
   /**
-   *
+   * 
    * @key `codeBeat.enable`
    * @default `true`
    * @type `boolean`
    */
   codeBeatEnable: {
-    key: 'codeBeat.enable',
+    key: "codeBeat.enable",
     default: true,
-  } as ConfigItem<'codeBeat.enable'>,
+  } as ConfigItem<"codeBeat.enable">,
 }
 
 export interface ScopedConfigKeyTypeMap {
 }
 
 export const scopedConfigs = {
-  scope: 'codbeat-ext',
+  scope: "codebeat-ext",
   defaults: {
   } satisfies ScopedConfigKeyTypeMap,
 }
 
 export interface NestedConfigs {
-  codeBeat: {
-    enable: boolean
-  }
+  "codeBeat": {
+    "enable": boolean,
+  },
 }
 
 export interface NestedScopedConfigs {
 }
+
